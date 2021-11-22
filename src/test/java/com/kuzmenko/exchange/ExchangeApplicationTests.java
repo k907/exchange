148ef71c87e0f6c1a9506c1ef234799ecca03801
Алексей <7019405@gmail.com>
2021-11-22 23:38:43 +0200

    project infrastructure created

---
+++ src/test/java/com/kuzmenko/exchange/ExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzmenko.exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
