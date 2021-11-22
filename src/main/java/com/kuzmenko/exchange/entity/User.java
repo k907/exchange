21c533e02e7bfca352186208deb89e3cf51a361f
Алексей <7019405@gmail.com>
2021-11-23 00:38:08 +0200

    repositories created

--- src/main/java/com/kuzmenko/exchange/entity/User.java
+++ src/main/java/com/kuzmenko/exchange/entity/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @Column(name = "first_name")
---
+++ src/main/java/com/kuzmenko/exchange/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.kuzmenko.exchange.repository;
+
+import com.kuzmenko.exchange.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
